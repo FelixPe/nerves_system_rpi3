e009c90310951a7eb6c64a6bdd3ad8ace5671ecb
Felix Penzlin <Felix.Penzlin@hilti.com>
2026-01-07 16:35:20 +0100

    add support for canbus using MCP2515

--- mix.exs
+++ mix.exs
@@ -1,8 +1,8 @@
-defmodule NervesSystemRpi3.MixProject do
+defmodule NervesSystemRpi3CanBus.MixProject do
   use Mix.Project
 
-  @github_organization "nerves-project"
-  @app :nerves_system_rpi3
+  @github_organization "FelixPe"
+  @app :nerves_system_rpi3_canbus
   @source_url "https://github.com/#{@github_organization}/#{@app}"
   @version Path.join(__DIR__, "VERSION")
            |> File.read!()
